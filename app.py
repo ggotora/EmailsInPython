dda574706030047ed3b16458ab36d79f3e365bb4
gilbertgotora2@gmail.com <gilbertgotora2@gmail.com>
2022-12-05 01:27:41 +0200

    Add usename and password and email server for login

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import smtplib
 import json
 f = open("secrets.json")
 secrets = json.load(f)
 print(secrets)
+
+email = """This is a new email saka so"""
+
+server = smtplib.SMTP_SSL('smtp.gmail.com')
+server.login(secrets["username"], secrets["password"])
+server.sendmail(secrets["username"], secrets["password"], email)
