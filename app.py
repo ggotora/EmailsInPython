f0f4e2427d443d7653bdd407eeb5efa81fc31509
gilbertgotora2@gmail.com <gilbertgotora2@gmail.com>
2022-12-05 03:45:24 +0200

    Add email message, and user credentials

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 import smtplib
+from email.message import EmailMessage
+import mimetypes
 import json
+import os 
+
 f = open("secrets.json")
 secrets = json.load(f)
-print(secrets)
 
-email = """This is a new email saka so"""
+# Setup email message 
+message = EmailMessage()
+sender = secrets["username"]
+recipient = secrets["username"]
+sender = "ispeakjavascript.gmail.com"
+message['From'] = sender
+message['To'] = recipient
+message["Subject"] = "Hello world"
+body = """Hey there!
+...
+... I'm learning to send emails using Python!"""
+message.set_content(body)
+print(message)
+
+# Attachment 
+attachment_path = "/tmp/processed.pdf"
+attachment_filename = os.path.basename(attachment_path)
+mime_type, _ = mimetypes.guess_type(attachment_path)
+mime_type, mime_subtype = mime_type.split('/', 1)
+
+# Add attachment to message 
+with open(attachment_path, 'rb') as ap:
+    message.add_attachment(ap.read(),
+                            maintype=mime_type,
+                         subtype=mime_subtype,
+                         filename=os.path.basename(attachment_path))
+
+print(message)
+
 
 server = smtplib.SMTP_SSL('smtp.gmail.com')
 server.login(secrets["username"], secrets["password"])
-server.sendmail(secrets["username"], secrets["password"], email)
+server.send_message(message)
+server.close()
